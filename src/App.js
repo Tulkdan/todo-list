bc6a5c3c7907bbc3a2435459cca0ab11c3b0d917
Pedro Correa <pedro8correa@gmail.com>
2019-07-08 14:02:29 -0300

    Removed old project and started a new one

--- src/App.js
+++ src/App.js
@@ -1,78 +1,26 @@
-import React, { Component } from 'react'
-import firebase from 'firebase'
-import Container from '@material-ui/core/Container'
-import NavBar from './components/NavBar'
-import LoggerInContainer from './components/LoggedInContainer'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  state = {
-    todoList: []
-  }
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      user: null,
-      todoList: []
-    }
-
-    this.addTodo = this.addTodo.bind(this)
-    this.removeTodo = this.removeTodo.bind(this)
-    this.handleSignIn = this.handleSignIn.bind(this)
-    this.handleSignOut = this.handleSignOut.bind(this)
-  }
-
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({ user })
-    })
-  }
-
-  handleSignIn() {
-    const provider = new firebase.auth.GoogleAuthProvider()
-    firebase.auth().signInWithPopup(provider)
-  }
-
-  handleSignOut() {
-    firebase.auth().signOut()
-  }
-
-  addTodo(value) {
-    const { todoList } = this.state
-    todoList.push(value)
-    this.setState({ todoList })
-  }
-
-  removeTodo(index) {
-    const { todoList } = this.state
-    todoList.splice(index, 1);
-    this.setState({ todoList })
-  }
-
-  render() {
-    let isLogged
-
-    if (this.user) {
-      isLogged = <LoggerInContainer
-        addTodo={this.addTodo}
-        todoList={this.state.todoList}
-        removeTodo={this.removeTodo} />
-    }
-
-    return (
-      <div>
-        <NavBar
-          user={this.state.user}
-          signIn={this.handleSignIn}
-          signOut={this.handleSignOut}
-        />
-        <Container maxWidth="md" id="content">
-          { isLogged }
-        </Container>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
